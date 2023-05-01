e386cd44f4fb6f53f47f946c20eff3cd1bc15168
Athena Saldanha <asaldanha@chicagobooth.edu>
2023-05-01 16:35:05 +0000

    photo details done without forms

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,12 @@ class PhotosController<ApplicationController
     render ({:template => "photos_templates/index.html.erb"})
   end
 
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({:id => url_id})
+    @the_photo = matching_photos.first
+
+    render ({:template => "photos_templates/show.html.erb"})
+  end
+
 end
